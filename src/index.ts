b4b3bb64905b82eb0d28a99e59ac718fedbfc15f
Evan Coulson <evan.m.coulson@gmail.com>
2022-11-14 16:10:10 -0800

    Fixed typing error in runtime version

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { Action } from '@the-standard/types';
 import { AsyncRuntimeClient } from './clients/runtimes/async-runtime-client';
 import { RuntimeClient } from './clients/runtimes/runtime-client';
 
-export function createRuntime<T>(logic: Action<T>) {
-    const client = new RuntimeClient();
+export function createRuntime<T>(logic: Action<T>): RuntimeClient<T> {
+    const client = new RuntimeClient<T>();
     return client.withLogic(logic);
 }
 
-export function createAsyncRuntime<T>(logic: Action<Promise<T>>) {
-    const client = new AsyncRuntimeClient();
+export function createAsyncRuntime<T>(
+    logic: Action<Promise<T>>
+): AsyncRuntimeClient<T> {
+    const client = new AsyncRuntimeClient<T>();
     return client.withLogic(logic);
 }
